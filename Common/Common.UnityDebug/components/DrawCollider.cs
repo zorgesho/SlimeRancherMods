28c1290e34dd7a192920c21b352c82c0d7bd8cda
zorgesho <53096970+zorgesho@users.noreply.github.com>
2022-01-28 08:03:47 +0300

    shared project Common.UnityDebug added

---
+++ Common/Common.UnityDebug/components/DrawCollider.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Common.UnityDebug
+{
+	class DrawColliders: MonoBehaviour
+	{
+		interface ISetCollider { void setCollider(Collider collider); }
+
+		abstract class DrawCollider<CL, WR>: MonoBehaviour, ISetCollider where CL: Collider where WR: DrawWire
+		{
+			protected CL collider;
+			Bounds lastBounds;
+
+			public void setCollider(Collider collider)
+			{
+				this.collider = collider as CL;
+				Debug.assert(this.collider != null);
+
+				lastBounds = collider.bounds;
+			}
+
+			protected WR drawWire;
+			protected abstract void updateProps();
+
+			void Awake() => drawWire = gameObject.AddComponent<WR>();
+			void Start() => updateProps();
+			void OnDestroy() => Destroy(drawWire);
+
+			void Update()
+			{
+				if (lastBounds == collider.bounds)
+					return;
+
+				lastBounds = collider.bounds;
+				updateProps();
+			}
+		}
+
+		class DrawColliderBox: DrawCollider<BoxCollider, DrawBox>
+		{
+			protected override void updateProps() => drawWire.setProps(collider.center, collider.size);
+		}
+
+		class DrawColliderSphere: DrawCollider<SphereCollider, DrawSphere>
+		{
+			protected override void updateProps() => drawWire.setProps(collider.center, collider.radius);
+		}
+
+		class DrawColliderCapsule: DrawCollider<CapsuleCollider, DrawCapsule>
+		{
+			protected override void updateProps() => drawWire.setProps(collider.center, collider.radius, collider.height, collider.direction);
+		}
+
+
+		readonly List<Component> wires = new();
+
+		static readonly Dictionary<Type, Type> types = new() // key: colliderType, value: componentType
+		{
+			{ typeof(BoxCollider), typeof(DrawColliderBox) },
+			{ typeof(SphereCollider), typeof(DrawColliderSphere) },
+			{ typeof(CapsuleCollider), typeof(DrawColliderCapsule) }
+		};
+
+		void Start()
+		{
+			foreach (var collider in gameObject.GetComponentsInChildren<Collider>(true))
+			{
+				if (!types.TryGetValue(collider.GetType(), out Type componentType))
+					continue;
+
+				var wire = collider.gameObject.AddComponent(componentType);
+				wires.Add(wire);
+
+				(wire as ISetCollider)?.setCollider(collider);
+			}
+		}
+
+		void OnDestroy() => wires.ForEach(Destroy);
+	}
+}
--- Common/UnityExtensions.cs
+++ Common/UnityExtensions.cs
@@ -10,5 +10,31 @@ namespace Common
 
 		public static GameObject getParent(this GameObject go) => go.transform.parent?.gameObject;
 		public static GameObject getChild(this GameObject go, string name) => go.transform.Find(name)?.gameObject;
+
+		public static void setTransform(this GameObject go, Vector3? pos = null, Vector3? localPos = null, Vector3? localAngles = null, Vector3? localScale = null)
+		{
+			var tr = go.transform;
+
+			if (pos != null)			tr.position = (Vector3)pos;
+			if (localPos != null)		tr.localPosition = (Vector3)localPos;
+			if (localAngles != null)	tr.localEulerAngles = (Vector3)localAngles;
+			if (localScale != null)		tr.localScale = (Vector3)localScale;
+		}
+
+		public static void setParent(this GameObject go, GameObject parent, Vector3? localPos = null, Vector3? localAngles = null)
+		{
+			go.transform.SetParent(parent.transform, false);
+			go.setTransform(localPos: localPos, localAngles: localAngles);
+		}
+
+		public static GameObject createChild(this GameObject go, string name, Vector3? localPos = null)
+		{
+			GameObject child = new (name);
+			child.setParent(go);
+
+			child.setTransform(localPos: localPos);
+
+			return child;
+		}
 	}
 }
