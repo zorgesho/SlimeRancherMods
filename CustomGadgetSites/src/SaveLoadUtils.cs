834a5857756192ac091ce19485a934f7632f1547
zorgesho <53096970+zorgesho@users.noreply.github.com>
2022-03-06 16:05:06 +0300

    fix for SRML bug

--- CustomGadgetSites/src/SaveLoadUtils.cs
+++ CustomGadgetSites/src/SaveLoadUtils.cs
@@ -1,7 +1,9 @@
 ﻿using System.Linq;
 
+using HarmonyLib;
 using UnityEngine;
 
+using SRML;
 using SRML.SR;
 using SRML.SR.SaveSystem;
 using SRML.SR.SaveSystem.Data;
@@ -30,10 +32,27 @@ namespace CustomGadgetSites
 			SaveRegistry.RegisterWorldDataPreLoadDelegate(data =>
 				GadgetSiteManager.loadSites(data.GetCompoundPiece(dataPieceName).DataList.Select(dataToSite)));
 
-			SRCallbacks.PreSaveGameLoad += _ => GadgetSiteManager.createLoadedSites();
+			// we're using publicized SRML assembly (because of the patch below)
+			static void onGameLoad(SceneContext _) => GadgetSiteManager.createLoadedSites();
+			typeof(SRCallbacks).GetEvent("PreSaveGameLoad").GetAddMethod().Invoke(null, new[] { (SRCallbacks.OnSaveGameLoadedDelegate)onGameLoad });
 
 			SaveRegistry.RegisterWorldDataSaveDelegate(data =>
 				data.SetPiece(dataPieceName, GadgetSiteManager.sites.Select(siteToData).ToHashSet()));
+		}
+
+		// fix for SRML bug (first save for a new world doesn't contains extended data)
+		[HarmonyPatch(typeof(ExtendedData), "Push")]
+		static class ExtendedData_Push_Patch
+		{
+			static readonly string modID = nameof(CustomGadgetSites).ToLower();
+
+			static void Prefix()
+			{
+				var modInfo = SRModLoader.Mods[modID];
+
+				if (!ExtendedData.worldSaveData.ContainsKey(modInfo))
+					ExtendedData.worldSaveData[modInfo] = new CompoundDataPiece("root");
+			}
 		}
 	}
 }
