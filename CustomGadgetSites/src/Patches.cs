365e51d1b41e57408b885933a468a9cd1fc0f200
zorgesho <53096970+zorgesho@users.noreply.github.com>
2022-03-07 17:41:25 +0300

    allow to create new sites with left click

---
+++ CustomGadgetSites/src/Patches.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace CustomGadgetSites
+{
+	[HarmonyPatch(typeof(TargetingUI), "Update")]
+	static class TargetingUI_Update_Patch
+	{
+		const float raycastDistance = 10f;
+
+		static WeaponVacuum vacpack;
+
+		static GameObject getPointedObject(out Vector3 position)
+		{
+			var tr = vacpack.vacOrigin.transform;
+			Physics.Raycast(tr.position, tr.up, out var hit, raycastDistance);
+			position = hit.point;
+
+			return hit.collider?.gameObject;
+		}
+
+		static void Postfix()
+		{
+			if (!vacpack)
+				vacpack = SRSingleton<SceneContext>.Instance.Player.GetComponentInChildren<WeaponVacuum>();
+
+			if (vacpack.vacMode != WeaponVacuum.VacMode.GADGET)
+				return;
+
+			if (getPointedObject(out var position) is not GameObject target)
+				return;
+
+			var targetSite = target.GetComponentInParent<GadgetSite>();
+
+			if (SRInput.Actions.attack.WasPressed)
+			{
+				if (!targetSite)
+					GadgetSiteManager.createSite(position);
+			}
+		}
+	}
+}
